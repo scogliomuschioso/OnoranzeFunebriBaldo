9e6f9b2d7b7b478fc4712619dcdacc223753dbe9
Antonio <baldo.antonio4@libero.it>
2026-05-16 17:48:30 +0200

    first commit ;)

---
+++ src/test/java/com/baldosrl/tambuto/TambutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baldosrl.tambuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TambutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
